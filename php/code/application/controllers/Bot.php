6f2edc4af4e5e02d821e6610de02d8a002a6db8c
hirekaanmicheal@gmail.com <hirekaanmicheal@gmail.com>
2017-01-17 13:12:57 +0100

    fifth commit

--- php/code/application/controllers/Bot.php
+++ php/code/application/controllers/Bot.php
@@ -32,5 +32,35 @@ class Bot extends CI_Controller {
 
 	# 	@route 	response/command
 	public function get_command() {}
+
+
+	/*
+	 db structure
+		- bot 
+			- id string
+			- network_details
+				- interface_name: string
+				- ip_address: string
+				- broadcast_address: string
+				- subnet_mask: string
+				- gateway_address: string
+			- status: STATUS
+			- host_url: string 
+			- sleep_cycle: int = 10
+			- sleep_cycle_randomness: int =5
+			- os
+				- name: string
+				- version: string
+				- type: string
+				- arch: string
+				- username string
+
+		- user
+			- id: int
+			- username: string
+			- password: string
+
+	*/
+
 }
 ?>
